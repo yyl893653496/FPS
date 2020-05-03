a801b5e14ca4de22ad58fdba1352256417091cc1
NSWell <paris3@163.com>
2020-05-03 21:24:30 +0800

    update project

---
+++ FPS-Multiplayer/Assets/Scripts/RagdollController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RagdollController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetKinematic(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    void SetKinematic(bool newValue)
+    {
+        Rigidbody[] bodies = GetComponentsInChildren<Rigidbody>();
+        foreach (Rigidbody rb in bodies)
+        {
+            rb.useGravity = !newValue;
+            rb.isKinematic = newValue;
+        }
+    }
+}
